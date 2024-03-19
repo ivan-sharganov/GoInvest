e28b4c5fa4c60ebe4e5695c022c488b4fcf8ff06
Ivan Sharganov <i.sharganov@vk.team>
2024-03-19 21:42:13 +0300

    Initial Commit

---
+++ GoInvest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoInvest
+//
+//  Created by i.sharganov on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
