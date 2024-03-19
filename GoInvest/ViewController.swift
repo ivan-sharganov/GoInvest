2ced4e8571543ab96d542b682c4ab0c80dd530f7
Ivan Sharganov <i.sharganov@vk.team>
2024-03-19 21:58:55 +0300

    create project without storyboard

--- GoInvest/ViewController.swift
+++ GoInvest/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  GoInvest
-//
-//  Created by i.sharganov on 19.03.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
