43e67aa46e9b298dac8d4afa974e9ceb7bf6780f
Ivan Sharganov <i.sharganov@vk.team>
2024-03-21 15:27:55 +0300

    #HEAD #main linted

--- GoInvest/AppDelegate.swift
+++ GoInvest/AppDelegate.swift
@@ -5,7 +5,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
 
         let window = UIWindow()
         window.rootViewController = ViewController()
@@ -15,4 +18,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 }
-
