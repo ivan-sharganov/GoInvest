25e2d4af9082ebe5089594eb683cb3d29d31d116
BUSLIGHTER <80523182+Buslighter@users.noreply.github.com>
2024-03-23 00:11:55 +0300

    39-add-network-layer-2
    SwiftLintFix

--- GoInvest/NetworkManager.swift
+++ GoInvest/NetworkManager.swift
@@ -1,15 +1,14 @@
 import Foundation
 
-class NetworkManager{
-    
+class NetworkManager {
+
     let shared = NetworkManager()
 
-    
     func transformPriceData(from initialData: [[Datum]]) -> PricesData {
         var outD = [PricesModel]()
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yy-MM-dd"
-        for (_, data) in initialData.enumerated(){
+        for (_, data) in initialData.enumerated() {
             var price = PricesModel()
             data.forEach({ (element) in
                 if case .integer(let integer) = element {
@@ -27,25 +26,25 @@ class NetworkManager{
         }
         return PricesData(data: outD)
     }
-    
+
     func transformStockData(from initialData: [[Datum]]) -> StockData {
         var outD = [StockModel]()
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yy-MM-dd"
-        for (index, data) in initialData.enumerated(){
+        for (index, data) in initialData.enumerated() {
             var price = StockModel()
             data.forEach({ (element) in
                 if case .double(let double) = element {
-                    if(index==2){
+                    if index==2 {
                         price.close = double
-                    }else{
+                    } else {
                         price.trendclspr = double
                     }
                 }
                 if case .string(let string) = element {
-                    if(index==0){
+                    if index==0 {
                         price.shortName = string
-                    }else{
+                    } else {
                         price.ticker = string
                     }
                 }
@@ -54,17 +53,16 @@ class NetworkManager{
         }
         return StockData(data: outD)
     }
-    
-    
+
     func getPricesForTicker() {
         let url = "https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/YNDX/securities.json?iss.only=securities&from=2024-03-11&till=2024-03-19&interval=2&iss.meta=off&history.columns=CLOSE,VOLUME,TRADEDATE"
 
         var request = URLRequest(url: URL(string: url)!)
         request.httpMethod = "GET"
 
-        URLSession.shared.dataTask(with: request){ data, response, error in
+        URLSession.shared.dataTask(with: request) { data, _, _ in
             DispatchQueue.main.async {
-                if let data = data, let answer = try? JSONDecoder().decode(Response.self, from: data){
+                if let data = data, let answer = try? JSONDecoder().decode(Response.self, from: data) {
                     print(self.transformPriceData(from: answer.history.data)) // передача дальше
                 } else {
                     print("NO DATA")
@@ -72,15 +70,15 @@ class NetworkManager{
             }
         }.resume()
     }
-    
+
     func getPricesForStock() {
         let url = "https://iss.moex.com/iss/history/engines/stock/markets/shares/sessions/3/securities.json?iss.only=securities&iss.meta=off&history.columns=SHORTNAME,SECID,CLOSE,TRENDCLSPR,BOARDID&limit=20&start=0"
 
         var request = URLRequest(url: URL(string: url)!)
         request.httpMethod = "GET"
-        URLSession.shared.dataTask(with: request){ data, response, error in
+        URLSession.shared.dataTask(with: request) { data, _, _ in
             DispatchQueue.main.async {
-                if let data = data, let answer = try? JSONDecoder().decode(Response.self, from: data){
+                if let data = data, let answer = try? JSONDecoder().decode(Response.self, from: data) {
                     print(self.transformStockData(from: answer.history.data)) // передача дальше
                 } else {
                     print("NO DATA")
@@ -88,5 +86,5 @@ class NetworkManager{
             }
         }.resume()
     }
-    
+
 }
--- GoInvest/PricesAPIModel.swift
+++ GoInvest/PricesAPIModel.swift
@@ -1,45 +1,44 @@
 import Foundation
 
-struct PricesData{
-    
+struct PricesData {
+
     let data: [PricesModel]
-    
+
 }
 struct PricesModel {
-    
+
     /// Цена закрытия
-    var close : Double?
-    
+    var close: Double?
+
     /// Количество продаж
-    var volume : Int?
-    
+    var volume: Int?
+
     /// Дата
     var date: Date?
-    
+
 }
 
-
 struct Response: Decodable {
-    
+
     let history: History
-    
+
 }
 
 struct History: Decodable {
-    
+
     /// Колонки(ключи джейсона)
     let columns: [String]
-    
-    ///Дата(массив непонятно чего)
+
+    /// Дата(массив непонятно чего)
     let data: [[Datum]]
-    
+
 }
 enum Datum: Decodable {
     case double(Double)
     case string(String)
     case integer(Int)
     case null
-    
+
     init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         if let x = try? container.decode(Int.self) {
