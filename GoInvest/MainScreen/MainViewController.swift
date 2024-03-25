8b4e80570c4f5802dddf9b2c383199619d332d11
BUSLIGHTER <80523182+Buslighter@users.noreply.github.com>
2024-03-25 22:21:59 +0300

    37 hook

--- GoInvest/MainScreen/MainViewController.swift
+++ GoInvest/MainScreen/MainViewController.swift
@@ -115,7 +115,7 @@ final class MainViewController: UIViewController {
     private func setupTabBarItem() {
         let imageSize = CGSize(width: 29, height: 22)
         let imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
-
+ 
         let image = UIImage(named: "list.bullet")?
             .withSize(imageSize)
         let selectedImage = UIImage(named: "list.bullet.selected")?
