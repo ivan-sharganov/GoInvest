c1875a1375abe671a226bf91163821f0a45d1af8
unknownkirya <unknownkirya@gmail.com>
2024-03-25 13:58:35 +0300

    IN PROGRESS: fav button

--- GoInvest/DetailScreen/DetailViewController.swift
+++ GoInvest/DetailScreen/DetailViewController.swift
@@ -7,7 +7,47 @@ final class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
+    }
+    
+    // MARK: - Private methods
+    
+    private func setupUI() {
         view.backgroundColor = .white
+        
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        let addToFavoritesButton = UIImage(named: "addToFavoritesButton")
+        let button = UIButton(type: .custom)
+        
+//        addToFavoritesButton.frame
+        button.setImage(addToFavoritesButton, for: .normal)
+        button.contentMode = .center
+//        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        button.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        
+        let rightBarButtonItem = UIBarButtonItem(customView: button)
+//        let rightBarButtonItem = UIBarButtonItem(image: addToFavoritesButton, style: .plain, target: self, action: #selector(rightBarButtonItemTapped))
+        
+//        let sdsd = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: nil)
+        
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+        
+    }
+    
+    // MARK: - Handlers
+    
+    @objc
+    private func rightBarButtonItemTapped(_ sender: UIBarButtonItem) {
+        sender.image = UIImage(named: "inFavoritesButton")?.withRenderingMode(.alwaysOriginal)
     }
 
+}
+
+@propertyWrapper
+struct PhoneNumber {
+    var wrappedValue: String
+    
 }
