71ee153b90508ab1ec4ab0bf2ae970324f32a445
BUSLIGHTER <80523182+Buslighter@users.noreply.github.com>
2024-03-28 13:28:09 +0300

    117 added caching

--- GoInvest/Services/FileManager.swift
+++ GoInvest/Services/FileManager.swift
@@ -1,13 +1,51 @@
-// import Foundation
-//
-// class CachingClass{
-//
-//    let fileManager = FileManager.default
-//    let filePath = "myfile.txt"
-//
-//    if fileManager.fileExists(atPath: filePath) {
-//        print("File exists")
-//    } else {
-//        print("File does not exist")
-//    }
-// }
+import Foundation
+
+final class CachingClass {
+    static let shared = CachingClass()
+    
+    func getDocumentDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func saveCache(records: StockData, key: String) {
+        let fileName = "CacheFile" + key
+        /// Задаем путь к файлы с кэшем
+        let filePath = getDocumentDirectory().appendingPathComponent(fileName)
+        do {
+            /// Архивируем дату
+            let archiver = NSKeyedArchiver(requiringSecureCoding: false)
+            archiver.outputFormat = .binary
+            try archiver.encodeEncodable(records, forKey: key)
+            archiver.finishEncoding()
+            /// Пробуем записать дату в файл по URL
+            try archiver.encodedData.write(to: filePath)
+        } catch let error {
+            print("Failed to save cache: ", error.localizedDescription)
+        }
+    }
+    
+    func getCache(for key: String) -> StockData? {
+        let fileName = "CacheFile" + key
+        let filePath = self.getDocumentDirectory().appendingPathComponent(fileName)
+        
+        guard let data = try? Data(contentsOf: filePath) else {
+            print("Failed to get  cache")
+            
+            return nil
+        }
+        
+        do {
+            let unarchiver = try NSKeyedUnarchiver(forReadingFrom: data)
+            unarchiver.requiresSecureCoding = false
+            let cachedData = unarchiver.decodeDecodable(StockData.self, forKey: key)
+            
+            return cachedData
+        } catch let error {
+            print("Failed to get cache: ", error.localizedDescription)
+            
+            return nil
+        }
+    }
+    
+}
